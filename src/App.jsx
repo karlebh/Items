fd164231031f6abf7a7c4df95acdbd6580068f0b
karlebh <akejucaleb@gmail.com>
2023-01-05 04:34:41 -1100

    Made changes to app min height css

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,9 @@ window.gsap = gsap
 
 function App() {
   return (
-    <div className="bg-gradient-to-r from-emerald-700 to-teal-700">
+    <div className="bg-gradient-to-r from-emerald-700 to-teal-700 min-h-screen">
       <BrowserRouter>
-        <div className="min-h-screen max-w-7xl mx-auto">
+        <div className="max-w-7xl mx-auto">
           <ScrollToTop />
 
           <Header />
